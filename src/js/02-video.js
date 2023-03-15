6b4af31425421a6e50d0711bea3cc182c378a5ae
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-03-15 11:42:40 +0100

    Commit now

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
 
+player.on('play', onPlay);
+
+// If later on you decide that you don’t need to listen for play anymore.
+player.off('play', onPlay);
+
+// Alternatively, `off` can be called with just the event name to remove all
+// listeners.
+player.off('play');
