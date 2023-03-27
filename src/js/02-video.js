253fab18a00db730c97bd5d5dd6f7b34c537c209
EmiliaSzczepaniak52 <emiliaszczepaniak52@gmail.com>
2023-03-27 23:54:57 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -24,11 +24,4 @@ player
         break;
     }
 });
-player.on('play', onPlay);
 
-// If later on you decide that you don’t need to listen for play anymore.
-player.off('play', onPlay);
-
-// Alternatively, `off` can be called with just the event name to remove all
-// listeners.
-player.off('play');
